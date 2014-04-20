0d2918b060da49bfdff4e84ebb58a4c07cbe6f3f
Wolfgang Barth <wob@swobspace.net>
2014-04-20 12:45:05 +0200

    add capybara for integration testing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,8 +3,14 @@ ENV["RAILS_ENV"] = "test"
 
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 require "rails/test_help"
+require 'capybara/rails'
 
 Rails.backtrace_cleaner.remove_silencers!
+
+# load locales from config/locales
+Dir["#{File.dirname(__FILE__)}/../config/locales/**/*.yml"].each do |f|
+  I18n.load_path << f
+end
 
 # Load support files
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
@@ -12,4 +18,9 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 # Load fixtures from the engine
 if ActiveSupport::TestCase.method_defined?(:fixture_path=)
   ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
+end
+
+class ActionDispatch::IntegrationTest
+  # Make the Capybara DSL available in all integration tests
+  include Capybara::DSL
 end
--- wobapphelpers.gemspec
+++ wobapphelpers.gemspec
@@ -23,4 +23,5 @@ Gem::Specification.new do |s|
   s.add_development_dependency 'sass-rails', '~> 4.0.3'
   s.add_development_dependency "bootstrap-sass", "~> 3.1.1"
   s.add_development_dependency "sqlite3"
+  s.add_development_dependency "capybara"
 end
