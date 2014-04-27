23c22f2dcc125e65d80958f66418889df6f4540e
Wolfgang Barth <wob@swobspace.net>
2014-04-27 12:26:54 +0200

    add Wobapphelpers.setup for configuration

--- lib/wobapphelpers.rb
+++ lib/wobapphelpers.rb
@@ -1,4 +1,28 @@
 module Wobapphelpers
   autoload :Helpers, 'wobapphelpers/helpers'
   autoload :Responders, 'wobapphelpers/responders'
+
+  def self.setup
+    yield self
+  end
+
+
+  # Use cancan abilities to decide if action links should be presented. 
+  # Defaults to :none
+  #
+  mattr_reader :cancan 
+  @@cancan = :none
+
+  # Which variant of cancan should be used? possible values are:
+  #   * +:none+: no usage of Cancan or Cancancan
+  #   * +:cancan1+: cancan '~> 1.6.0' or cancancan '~> 1.7.0'
+  #   * +:cancan2+: cancan '~> 2.0.0'
+  #
+  def self.cancan=(variant)
+    if [:none, :cancan1, :cancan2].include?(variant)
+      @@cancan = variant
+    else
+      raise "Cancan variant #{variant} not implemented!"
+    end
+  end
 end
--- test/wobapphelpers_test.rb
+++ test/wobapphelpers_test.rb
@@ -4,4 +4,17 @@ class WobapphelpersTest < ActiveSupport::TestCase
   test "truth" do
     assert_kind_of Module, Wobapphelpers
   end
+
+  [:cancan1, :cancan2, :none].each do |variant|
+    test "configure cancan to #{variant.to_s}" do
+      Wobapphelpers.setup {|config| config.cancan = variant}
+      assert_equal variant, Wobapphelpers::cancan
+    end
+  end
+
+  test "configure raise error if cancan is invalid" do
+    assert_raises RuntimeError do 
+      Wobapphelpers.setup {|config| config.cancan = :invalid}
+    end
+  end
 end
