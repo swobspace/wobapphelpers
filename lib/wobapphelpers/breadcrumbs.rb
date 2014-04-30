a7356952754375182bb5fd8ef8c389a69a7a198f
Wolfgang Barth <wob@swobspace.net>
2014-04-30 13:12:02 +0200

    prefer manual load from ApplicationController

--- lib/wobapphelpers/breadcrumbs.rb
+++ lib/wobapphelpers/breadcrumbs.rb
@@ -5,7 +5,5 @@ module Wobapphelpers
     
     ::ActionController::Base.send :include, Wobapphelpers::Breadcrumbs::ActionController
     ::ActionView::Base.send :include, Wobapphelpers::Breadcrumbs::BreadcrumbsHelper
-    ::ActionController::Base.send :before_action, "add_breadcrumb_index", only: :index
-    ::ActionController::Base.send :append_before_action, "add_breadcrumb_show", only: :show
   end
 end
