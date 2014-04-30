80c76e5d6d05530201cc9f03ef95387a750ec055
Wolfgang Barth <wob@swobspace.net>
2014-04-30 16:35:58 +0200

    rewrite with ActiveSupport::Concern

--- lib/wobapphelpers/breadcrumbs.rb
+++ lib/wobapphelpers/breadcrumbs.rb
@@ -1,9 +1,16 @@
+require 'active_support/concern'
+
 module Wobapphelpers
   module Breadcrumbs
+    extend ActiveSupport::Concern
+
     autoload :ActionController, 'wobapphelpers/breadcrumbs/action_controller.rb'
     autoload :BreadcrumbsHelper, 'wobapphelpers/breadcrumbs/breadcrumbs_helper.rb'
     
-    ::ActionController::Base.send :include, Wobapphelpers::Breadcrumbs::ActionController
-    ::ActionView::Base.send :include, Wobapphelpers::Breadcrumbs::BreadcrumbsHelper
+    included do
+      include Wobapphelpers::Breadcrumbs::ActionController
+    end
   end
 end
+
+ActionView::Base.send :include, Wobapphelpers::Breadcrumbs::BreadcrumbsHelper
